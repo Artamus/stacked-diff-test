f3f4dc45c7f635c44441e56ca0203754d10781df
Markus Loide <markusloide@gmail.com>
2024-04-28 23:59:20 +0300

    Reset source.

--- src/TestMain.kt
+++ src/TestMain.kt
@@ -1,9 +1,4 @@
 fun main() {
-  println("Test2")
-  println("Test4")
-  println("feature-branch-11")
-  println("feature-branch-2")
-  println("raw-stack-1-1")
-  println("raw-stack-2")
+  println("hello, world")
 }
 
