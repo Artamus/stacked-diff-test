14b2ca715dc9458eb6995ffa4a6074416eaf8b37
Markus Loide <markusloide@gmail.com>
2024-05-09 23:21:44 +0300

    cleanup

--- src/TestMain.kt
+++ src/TestMain.kt
@@ -1,23 +1,3 @@
 fun main() {
   println("hello, world")
-  println("Maiao 1-1")
-  println("Maiao 2")
-  println("Maiao 3")
-  println("Maiao 4")
-  println("Maiao 5")
-  println("Maiao 6")
-  println("Maiao 7")
-  println("Maiao 8")
-  println("Maiao 9-1")
-  println("Maiao 10-1")
-  println("rawgit 1")
-  println("rawgit 1-1")
-  println("rawgit 2")
-  println("rawgit 2-1")
-  println("rawgit 2-2")
-  println("rawgit 3")
-  println("machete 1-1")
-  println("machete 2")
-  println("machete 3")
 }
-
